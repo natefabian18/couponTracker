eb4b7c57e45335d0532844f86434e146774cdce5
secrjosh <joshua.secrist@stvincent.edu>
2020-04-22 19:43:50 -0400

    search page layout and search bar, removed search from home page and now on the bar accross the top

---
+++ CouponTrackerWebsite/Pages/search/Search.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CouponTrackerWebsite.Data;
+
+namespace CouponTrackerWebsite.Pages.coupon
+{
+    public class SearchModel : PageModel
+    {
+        private readonly CouponTrackerWebsite.Data.ApplicationDbContext _context;
+
+        public SearchModel(CouponTrackerWebsite.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CouponTrackerWebsite.Data.coupon> coupon { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            coupon = await _context.coupon.ToListAsync();
+            System.Diagnostics.Debug.WriteLine("*");
+            System.Diagnostics.Debug.WriteLine(SearchString);
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+    }
+}
