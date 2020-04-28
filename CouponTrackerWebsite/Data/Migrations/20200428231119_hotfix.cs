7cccfc881fdcf582eb5f35d5b44f6a33b927263f
Nate Fabian <nathan.fabian@stvincent.edu>
2020-04-28 19:46:29 -0400

    minor css tweaks

---
+++ CouponTrackerWebsite/Data/Migrations/20200428231119_hotfix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CouponTrackerWebsite.Data.Migrations
+{
+    public partial class hotfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                migrationBuilder.AddColumn<int>(
+                name: "ApplicationUser",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationUser",
+                table: "AspNetUsers");
+        }
+    }
+}
